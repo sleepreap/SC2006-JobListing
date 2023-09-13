5e8e4c49e44be31b75f1205d1e7cb98e87dfd7cd
sleepreap <98008874+sleepreap@users.noreply.github.com>
2023-09-14 00:01:49 +0800

    add map markers

--- frontend/src/pages/ViewMap.js
+++ frontend/src/pages/ViewMap.js
@@ -1,10 +1,40 @@
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { useState } from "react";
+import { Icon } from "leaflet";
 
 function ViewMap() {
   const [lat, setLat] = useState("");
   const [long, setLong] = useState("");
+
+  const homeMarker = {
+    id: 1,
+    geocode: [lat, long],
+    popUp: "Current Location",
+  };
+
+  const jobMarkers = [
+    {
+      id: 2,
+      geocode: [1.3397, 103.7067],
+      popUp: "Job 1",
+    },
+    {
+      id: 3,
+      geocode: [1.3419, 103.6974],
+      popUp: "Job 2",
+    },
+  ];
+
+  const JobIcon = new Icon({
+    iconUrl: " https://cdn-icons-png.flaticon.com/512/2377/2377874.png",
+    iconSize: [38, 38],
+  });
+
+  const homeMarkerIcon = new Icon({
+    iconUrl: "https://cdn-icons-png.flaticon.com/512/684/684908.png",
+    iconSize: [38, 38],
+  });
 
   //use to get users current location to center map
   const handleClick = () => {
@@ -59,6 +89,18 @@ function ViewMap() {
             </div>
           }
         />
+        {jobMarkers.map((marker) => (
+          <Marker key={marker.id} position={marker.geocode} icon={JobIcon}>
+            <Popup>{marker.popUp}</Popup>
+          </Marker>
+        ))}
+        <Marker
+          key={homeMarker.id}
+          position={homeMarker.geocode}
+          icon={homeMarkerIcon}
+        >
+          <Popup>{homeMarker.popUp}</Popup>
+        </Marker>
       </MapContainer>
     </div>
   );
